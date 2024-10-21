416e3adf2360e31bf07917331469f205442d9928
Reynaldi Riva <reynaldi.riva@gmail.com>
2024-10-21 17:03:33 +0700

    refactor: modularize the header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -8,18 +8,30 @@ export const Header = () => {
   const { state } = useBlog();
   const pathname = usePathname();
 
+  const Logo = () => {
+    return (
+      <Link href={'/'}>
+        <span className='bg-clip-text text-transparent bg-gradient-to-r from-pink-600 to-violet-600 text-2xl font-extrabold'>
+          Blog
+        </span>
+      </Link>
+    );
+  };
+
+  const AddBlogPostButton = () => {
+    return (
+      <Link href='/blog/create-post'>
+        <Button>Add Blog Post</Button>
+      </Link>
+    );
+  };
+
   return (
     <div className='fixed top-0 left-0 z-10 p-4 px-8 w-full h-[var(--header-height)] bg-[#ebeaf2] shadow-md'>
       <div className='flex flex-row justify-between items-center h-full'>
-        <Link href={'/'}>
-          <span className='bg-clip-text text-transparent bg-gradient-to-r from-pink-600 to-violet-600 text-2xl font-extrabold'>
-            Blog
-          </span>
-        </Link>
+        <Logo />
         {state.posts.length && pathname !== '/blog/create-post' ? (
-          <Link href='/blog/create-post'>
-            <Button>Add Blog Post</Button>
-          </Link>
+          <AddBlogPostButton />
         ) : null}
       </div>
     </div>
